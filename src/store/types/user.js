0fe0772c77b533cfc4b243760b11040414441708
guoguowww818 <guoguowww818@126.com>
2018-11-09 10:08:55 +0800

    reformat for eslint

--- src/store/types/user.js
+++ src/store/types/user.js
@@ -1,6 +1,6 @@
 export const ASYNC_FETCH_USER = 'ASYNC_FETCH_USER'
-export const SET_USER_INFO = 'SET_USER_INFO';
-export const SET_USER_RECORDS = 'SET_USER_RECORDS';
-export const SET_TIP = 'SET_TIP';
-export const SET_USER_LEVEL = 'SET_USER_LEVEL';
-export const SET_RANK = 'SET_RANK';
+export const SET_USER_INFO = 'SET_USER_INFO'
+export const SET_USER_RECORDS = 'SET_USER_RECORDS'
+export const SET_TIP = 'SET_TIP'
+export const SET_USER_LEVEL = 'SET_USER_LEVEL'
+export const SET_RANK = 'SET_RANK'
