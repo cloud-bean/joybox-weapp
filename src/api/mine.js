d660070fe1c1f509172963d14a4a0b3c6872025b
guoguowww818 <guoguowww818@126.com>
2018-11-12 09:47:17 +0800

    update api in mine and wrold

--- src/api/mine.js
+++ src/api/mine.js
@@ -1,19 +1,12 @@
 import {fetchWithAccessToken, fetchWithAccessTokenV2} from './fetchKF'
 
-<<<<<<< HEAD
-=======
 // update avatar
 export const setMemberAvatar = (id, data) => fetchWithAccessToken(`/users/${id}/change/avatar`, 'put', data)
 
->>>>>>> fd759a8a153087a9d1a4b9402d4378405b1fe2d5
 // mine, my bag
 export const getAllMyCards = (page, limit) =>
   new Promise((resolve, reject) => {
-<<<<<<< HEAD
-    fetchWithAccessTokenV2('/myBag', 'get') // knapsack page --- 'GET /api/v2/myBag'
-=======
     fetchWithAccessToken(`/myBag?page=${page}&limit=${limit}`, 'get') // knapsack page --- 'GET /api/v2/myBag'
->>>>>>> fd759a8a153087a9d1a4b9402d4378405b1fe2d5
       .then((result) => {
         if (result.data && (result.data.code === 'success')) {
           resolve(result.data.data)
@@ -57,26 +50,6 @@ export const getKeepData = () =>
       })
   })
 
-<<<<<<< HEAD
-// timeline data
-export const getActivities = (page, limit) =>
-  new Promise((resolve, reject) => {
-    fetchWithAccessToken(`/timelines?page=${page}&limit=${limit}&sort=-created`, 'get')
-      .then(result => {
-        // if (result.data.code == 'success') {
-        //   const activities = result.data.data;
-        //   resolve(activities);
-        // }
-        resolve(result)
-      })
-      .catch(err => {
-        reject(err)
-      })
-  })
-=======
-
->>>>>>> fd759a8a153087a9d1a4b9402d4378405b1fe2d5
-
 export const upvoteActivity = id =>
   new Promise((resolve, reject) => {
     fetchWithAccessToken(`/timelines/${id}/addUpVote`, 'post')
@@ -94,13 +67,7 @@ export const upvoteActivity = id =>
 
 export const addMsgToActivity = (id, content) =>
   new Promise((resolve, reject) => {
-<<<<<<< HEAD
     fetchWithAccessToken(`/timelines/${id}/addMessage`, 'post', content)
-=======
-    fetchWithAccessToken(`/timelines/${id}/addMessage`, 'post', {
-      content
-    })
->>>>>>> fd759a8a153087a9d1a4b9402d4378405b1fe2d5
       .then(result => {
         if (result.data && (result.data.code === 'success')) {
           resolve(result.data.data)
--- src/api/world.js
+++ src/api/world.js
@@ -1,16 +1,5 @@
 import {fetchWithAccessToken} from './fetchKF'
 
-<<<<<<< HEAD
-// get the news.
-export const getNews = (page, limit) =>
-  new Promise((resolve, reject) => {
-    fetchWithAccessToken(`/documents?page=${page}&limit=${limit}`, 'get')
-      .then((result) => {
-        resolve({
-          data: result.data.data,
-          totalCount: result.header['x-total-items-count']
-        })
-=======
 // timeline data
 export const getAllTimelines = (page, limit) =>
   new Promise((resolve, reject) => {
@@ -24,7 +13,6 @@ export const getAllTimelines = (page, limit) =>
         } else {
           resolve([])
         }
->>>>>>> fd759a8a153087a9d1a4b9402d4378405b1fe2d5
       })
       .catch(err => {
         reject(err)
